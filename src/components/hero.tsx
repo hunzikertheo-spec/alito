8591c2ebc9ec1ca6b6b8762c72d4a0a83064f4b9
Théo Hunziker <theohunziker@MacBook-Pro-de-Theo.local>
2026-04-28 09:35:29 +0200

    fix: logo nav + favicon SVG triangle

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -37,8 +37,8 @@ export default function Hero() {
       {/* Navigation */}
       <nav className="relative z-10 flex items-center justify-between px-10 h-[72px]">
         <div className="flex items-center gap-2.5">
-          <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M10 2L19 18H1L10 2Z" fill="white"/>
+          <svg width="18" height="18" viewBox="0 0 20 20" fill="none">
+            <path d="M10 2L19 18H1L10 2Z" fill="white" />
           </svg>
           <span className="text-white font-semibold text-base tracking-wide">ALITO</span>
         </div>
